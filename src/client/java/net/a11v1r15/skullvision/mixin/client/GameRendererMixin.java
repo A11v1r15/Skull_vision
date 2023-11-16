6a785988e04d1464323d7ca062cdc55bf3552c6b
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2023-11-16 11:10:38 -0300

    :construction: It compiles!

--- src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
+++ src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
@@ -1,54 +1,60 @@
 package net.a11v1r15.skullvision.mixin.client;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.Block;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
-import net.minecraft.item.BlockItem;
+import net.minecraft.item.SkullItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.block.Block;
 import net.minecraft.util.Identifier;
 
 @Mixin(GameRenderer.class)
-public class GameRendererMixin implements AutoCloseable {
- 
-public void close(){}
+public abstract class GameRendererMixin implements AutoCloseable {
 
  @Shadow
-protected abstract void loadPostProcessor(Identifier);
+ protected abstract void loadPostProcessor(Identifier identifier);
 
 	@Inject(at = @At("TAIL"), method = "onCameraEntitySet(Lnet/minecraft/entity/Entity;)V")
-	private void skullVision$testEntityWearingSkull(CallbackInfo info, Entity entity) {
+	private void skullVision$testEntityWearingSkull(Entity entity, CallbackInfo info) {
 		if (entity instanceof LivingEntity){
 			ItemStack head = ((LivingEntity)entity).getEquippedStack(EquipmentSlot.HEAD);
 			if (head.isIn(ItemTags.NOTEBLOCK_TOP_INSTRUMENTS)){
 				Item headItem = head.getItem();
-				if (headItem instanceof BlockItem){
-					Block headBlock = ((BlockItem)headItem).getBlock();
-					if (headBlock instanceof SkullBlockEntity){
-						String soundName = ((SkullBlockEntity)headBlock).getNoteBlockSound().getPath();
-						String mobName = "";
-						for (String element : soundName.split(".")) {
-							if(!EntityType.get(element).isEmpty()){
-								mobName = element;
-								break;
-							}
-						}
-						this.loadPostProcessor(new Identifier("shaders/post/" + mobName + ".json"));
+				if (headItem instanceof SkullItem){
+					Block headBlock = ((SkullItem)headItem).getBlock();
+					Instrument headInstrument =	headBlock.getDefaultState().getInstrument();
+					String soundName = "";
+					if (headInstrument.hasCustomSound()){
+						soundName = SkullItem.getBlockEntityNbt(head).getString("note_block_sound");
+					} else {
+						soundName = headInstrument.getSound().value().getId().getPath();
 					}
+					String mobName = "";
+					for (String element : soundName.split(".")) {
+						if(!EntityType.get(element).isEmpty()){
+							mobName = element;
+							break;
+						}
+					}
+					this.loadPostProcessor(new Identifier("shaders/post/" + mobName + ".json"));
 				}
 			}
 		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		throw new UnsupportedOperationException("Unimplemented method 'close'");
 	}
 }
--- src/main/java/net/a11v1r15/skullvision/SkullVision.java
+++ src/main/java/net/a11v1r15/skullvision/SkullVision.java
@@ -17,6 +17,6 @@ public class SkullVision implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("The eyes are the window of the soul");
 	}
 }
