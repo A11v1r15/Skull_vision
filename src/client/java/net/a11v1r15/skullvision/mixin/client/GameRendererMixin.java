a58e105747a666b87b73cd1d1e78d1f9c2367616
Álvaro Carvalho <Alvaro_Carvalho@alu.ufc.br>
2023-11-16 03:20:16 -0300

    :bug: Wrong type conversions

--- src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
+++ src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
@@ -24,13 +24,13 @@ public class GameRendererMixin implements AutoCloseable {
 	@Inject(at = @At("TAIL"), method = "onCameraEntitySet(Lnet/minecraft/entity/Entity;)V")
 	private void skullVision$testEntityWearingSkull(CallbackInfo info, Entity entity) {
 		if (entity instanceof LivingEntity){
-			ItemStack head = (LivingEntity)entity.getEquippedStack(EquipmentSlot.HEAD);
+			ItemStack head = ((LivingEntity)entity).getEquippedStack(EquipmentSlot.HEAD);
 			if (head.isIn(ItemTags.NOTEBLOCK_TOP_INSTRUMENTS)){
 				Item headItem = head.getItem();
 				if (headItem instanceof BlockItem){
-					Block headBlock = (BlockItem)headItem.getBlock();
+					Block headBlock = ((BlockItem)headItem).getBlock();
 					if (headBlock instanceof SkullBlockEntity){
-						String soundName = (SkullBlockEntity)headBlock.getNoteBlockSound().getPath();
+						String soundName = ((SkullBlockEntity)headBlock).getNoteBlockSound().getPath();
 						String mobName = "";
 						for (String element : soundName.split(".")) {
 							if(EntityType.get(element) != Optional.empty()){
