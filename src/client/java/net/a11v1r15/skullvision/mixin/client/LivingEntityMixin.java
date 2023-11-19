d00a05995daa47b9673a502ad30f45953a523205
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2023-11-19 01:23:19 -0300

    :bug:

--- src/client/java/net/a11v1r15/skullvision/mixin/client/LivingEntityMixin.java
+++ src/client/java/net/a11v1r15/skullvision/mixin/client/LivingEntityMixin.java
@@ -1,10 +1,12 @@
 package net.a11v1r15.skullvision.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.a11v1r15.skullvision.SkullVision;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -20,6 +22,9 @@ extends Entity {
   public LivingEntityMixin(EntityType < ? > type, World world) {
     super(type, world);
   }
+
+  @Shadow
+  protected abstract ItemStack getEquippedStack(EquipmentSlot slot);
 
   @Inject(at = @At(value = "RETURN"), method =
     "onEquipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)V")
