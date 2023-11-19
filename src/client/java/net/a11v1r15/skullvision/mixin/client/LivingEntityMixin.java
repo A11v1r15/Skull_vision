3cfc7d82fc4b4085246cd0cc89d2b4a28d9591bd
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2023-11-18 22:17:56 -0300

    Maybe both?

--- src/client/java/net/a11v1r15/skullvision/mixin/client/LivingEntityMixin.java
+++ src/client/java/net/a11v1r15/skullvision/mixin/client/LivingEntityMixin.java
@@ -1,13 +1,11 @@
 package net.a11v1r15.skullvision.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.Perspective;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
@@ -16,21 +14,18 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-@Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity {
-  public PlayerEntityMixin(EntityType <? extends LivingEntity> type, World world) {
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin
+extends Entity {
+  public LivingEntityMixin(EntityType < ? > type, World world) {
     super(type, world);
   }
 
-  @Shadow
-  protected abstract boolean isMainPlayer();
-
   @Inject(at = @At(value = "RETURN"), method =
-    "equipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;)V")
-  private void skullVision$checkEquippingSkull(EquipmentSlot slot, ItemStack stack, CallbackInfo info) {
-    if (this.isMainPlayer() && slot == EquipmentSlot.HEAD) {
-    Perspective perspective = MinecraftClient.getInstance().options.getPerspective();
-    if (perspective.isFirstPerson()) {
+    "onEquipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)V")
+  private void skullVision$checkEquippingSkull(EquipmentSlot slot, ItemStack oldStack, ItemStack newStack, CallbackInfo info) {
+    if ((Object) this instanceof PlayerEntity && ((PlayerEntity)(Object) this).isMainPlayer()) {
+      if (slot == EquipmentSlot.HEAD) {
         MinecraftClient.getInstance().gameRenderer.onCameraEntitySet(this);
       }
     }
