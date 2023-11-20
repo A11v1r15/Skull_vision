b0b8bde5330647fc93a076765c173b212d17decf
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2023-11-20 15:36:12 -0300

    :zap: Call onCameraEntitySet only when entity or skull is changed

--- src/client/java/net/a11v1r15/skullvision/SkullVisionClient.java
+++ src/client/java/net/a11v1r15/skullvision/SkullVisionClient.java
@@ -1,10 +1,51 @@
 package net.a11v1r15.skullvision;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SkullItem;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.tag.ItemTags;
 
 public class SkullVisionClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+  public static String[] lastPostProcessors = {"null", "null"};
+
+  public static String getSkullNameFrom(Entity entity) {
+    if (entity instanceof LivingEntity) {
+      ItemStack skull = ((LivingEntity) entity).getEquippedStack(EquipmentSlot.HEAD);
+      if (skull.isIn(ItemTags.NOTEBLOCK_TOP_INSTRUMENTS)) {
+        Item skullItem = skull.getItem();
+        if (skullItem instanceof BlockItem) {
+          Block skullBlock = ((BlockItem) skullItem).getBlock();
+          Instrument skullInstrument = skullBlock.getDefaultState().getInstrument();
+          String soundName = "";
+          if (skullInstrument.hasCustomSound()) {
+            NbtCompound skullNBT = SkullItem.getBlockEntityNbt(skull);
+            if (skullNBT != null)
+              soundName = skullNBT.getString("note_block_sound");
+          } else {
+            soundName = skullInstrument.getSound().value().getId().getPath();
+          }
+          for (String element: soundName.split("\\.")) {
+            if (!EntityType.get(element).isEmpty()) {
+              return element;
+            }
+          }
+        }
+      }
+    }
+    return "null";
+  }
+
+  @Override
+  public void onInitializeClient() {
+    // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+  }
 }
--- src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
+++ src/client/java/net/a11v1r15/skullvision/mixin/client/GameRendererMixin.java
@@ -6,21 +6,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.enums.Instrument;
 import net.a11v1r15.skullvision.SkullVision;
-import net.minecraft.block.Block;
+import net.a11v1r15.skullvision.SkullVisionClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.SkullItem;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
@@ -39,49 +30,35 @@ public abstract class GameRendererMixin implements AutoCloseable {
   @Shadow
   protected abstract void onCameraEntitySet(Entity entity);
 
-
   @Inject(at = @At("RETURN"), method = "onCameraEntitySet(Lnet/minecraft/entity/Entity;)V")
   private void skullVision$testEntityWearingSkull(Entity entity, CallbackInfo info) {
-    //SkullVision.LOGGER.info("Function onCameraEntitySet called on " + ((entity != null) ? EntityType.getId(entity.getType()).getPath() : "null"));
-    Identifier entityShaderId = new Identifier("shaders/post/" + ((entity != null) ? EntityType.getId(entity.getType()).getPath() : "null") + ".json");
-      this.resourceManager.getResource(entityShaderId).ifPresent
-        (r -> {this.loadPostProcessor(entityShaderId); SkullVisionClient.lastPostProcessor = entityShaderId;});
+    String entityName = ((entity != null) ? EntityType.getId(entity.getType()).getPath() : "null");
+    String skullName = SkullVisionClient.getSkullNameFrom(entity);
+    SkullVision.LOGGER.info("Function onCameraEntitySet called on " + entityName);
 
-    if (entity instanceof LivingEntity) {
-      ItemStack head = ((LivingEntity) entity).getEquippedStack(EquipmentSlot.HEAD);
-      if (head.isIn(ItemTags.NOTEBLOCK_TOP_INSTRUMENTS)) {
-        Item headItem = head.getItem();
-        if (headItem instanceof BlockItem) {
-          Block headBlock = ((BlockItem) headItem).getBlock();
-          Instrument headInstrument = headBlock.getDefaultState().getInstrument();
-          String soundName = "";
-          if (headInstrument.hasCustomSound()) {
-            NbtCompound headNBT = SkullItem.getBlockEntityNbt(head);
-            if (headNBT != null)
-              soundName = headNBT.getString("note_block_sound");
-          } else {
-            soundName = headInstrument.getSound().value().getId().getPath();
-          }
-          String mobName = "";
-          for (String element: soundName.split("\\.")) {
-            if (!EntityType.get(element).isEmpty()) {
-              mobName = element;
-              break;
-            }
-          }
-          //SkullVision.LOGGER.info("soundName is \"" + soundName + "\" and mobName is \"" + mobName + "\"");
-          Identifier headShaderId = new Identifier("shaders/post/" + mobName + ".json");
-            this.resourceManager.getResource(headShaderId).ifPresentOrElse (
-              rr -> {this.loadPostProcessor(headShaderId); SkullVisionClient.lastPostProcessor = headShaderId;},
-              () -> { SkullVision.LOGGER.warn("No post shader found in " + headShaderId); }
-            );
-        }
-      }
-    }
+    Identifier entityShaderId = new Identifier("shaders/post/" + entityName + ".json");
+    Identifier skullShaderId = new Identifier("shaders/post/" + skullName + ".json");
+    String[] postProcessors = {entityName, skullName};
+    SkullVisionClient.lastPostProcessors = postProcessors;
+    //SkullVision.LOGGER.info(entityName + " " + SkullVisionClient.lastPostProcessors[0] + " / " + skullName + " " + SkullVisionClient.lastPostProcessors[1]);
+    this.resourceManager.getResource(skullShaderId).ifPresentOrElse(
+      r -> { this.loadPostProcessor(skullShaderId); },
+      () -> { this.resourceManager.getResource(entityShaderId).ifPresent(
+        s -> { this.loadPostProcessor(entityShaderId); }
+      );}
+    );
   }
 
   @Inject(at = @At(value = "RETURN"), method = "tick()V")
   private void skullVision$tickEquippingSkull(CallbackInfo info) {
-    this.onCameraEntitySet(this.client.options.getPerspective().isFirstPerson() ? this.client.getCameraEntity() : null);
+    if (this.client.options.getPerspective().isFirstPerson()) {
+      Entity entity = this.client.getCameraEntity();
+      String entityName = ((entity != null) ? EntityType.getId(entity.getType()).getPath() : "null");
+      String skullName = SkullVisionClient.getSkullNameFrom(entity);
+      
+      //SkullVision.LOGGER.info(entityName + " " + SkullVisionClient.lastPostProcessors[0] + " / " + skullName + " " + SkullVisionClient.lastPostProcessors[1]);
+      if (entity != null && (!entityName.equals(SkullVisionClient.lastPostProcessors[0]) || !skullName.equals(SkullVisionClient.lastPostProcessors[1])))
+        this.onCameraEntitySet(entity);
+    }
   }
 }
